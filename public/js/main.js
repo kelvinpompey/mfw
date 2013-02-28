9f6c49121c8a0ba5b51f78be03a7ceca473b5431
Kelvin Pompey <kelvin.pompey@gmail.com>
2013-02-28 11:44:02 -0500

    loose ends

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
-$(document).ready(function() {
+require(['jquery',
+        'libs/bootstrap/js/bootstrap-transition', 
+        'libs/bootstrap/js/bootstrap-carousel', 
+        'domReady!'], function($){
     $('#coverimage').carousel({
         interval: 5000,
         cycle: true
-    });
+    });    
 });
